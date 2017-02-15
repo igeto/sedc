0ac37488e642b0764b02029b49eb3a40ee0e26c3
Igor Igeto Mitkovski <igetom@gmail.com>
2017-02-15 15:55:24 +0100

    15.02.2017

--- Advanced JavaScript/test/practice/js/script.js
+++ Advanced JavaScript/test/practice/js/script.js
@@ -1,48 +1,115 @@
 let $container = $('#container');
 let recipes = [];
+let showIngredients = false;
+let showPreparation = false;
 
 class Recipe {
-	constructor(name, origin) {
-		this.name = name;
-		this.origin = origin;
-		this.ingredients = {};
-	}
-	addIngredient(ingredient) {
+    constructor(name, origin) {
+        this.name = name;
+        this.origin = origin;
+        this.ingredients = {};
+    }
+    addIngredient(ingredient) {
 
-	}
-}	
-$('#container').on('click', '#addRecipe', () => {
-	console.log('im clicked');
-	recipes.push(new Recipe($('#name').val(), $('#origin').val()));
-	// recipes[length - 1].addIngredient
-});
+    }
+}
 
-let newRecipe = () => {	
-	$container.append(
-		`<h1>New Recipe</h1>
+
+let newRecipe = () => {
+    $('#containerList').addClass('hide');
+    $container.append(
+        `<h1>New Recipe</h1>
 		<label for='name'>Name: </label> 
 		<input id='name' type='text'><br>
 		<label for='origin'>Origin:</label>
 		<input id='origin' type='text'><br>
-		<h3>Ingredients:</h3><br>
-		<select>
-			<option selected disabled>Ingredients</option>
-			<option value="">Milk</option>
-			<option value="">Eggs</option>
-			<option value="">Flour</option>
-			<option value="">Tomatoes</option>
-			<option value="">Potatoes</option>
-			<option value="">Paprika</option>
-			<option value="">White Cheese</option>
-			<option value="">Yellow Cheese</option>
-			<option value="">Sugar</option>
-			<option value="">Oil</option>
-			<option value="">Salt</option>
-			<option value="">Meat</option>
-		</select>
-		<input type='number' style='width: 100px'><br />
-		<button id='getIngredient'>Add Ingredient</button>
 		<button id='addRecipe'>Add Recipe</button> `
-	);
+    );
 };
-newRecipe();
+
+
+let getIngredients = () => {
+    showIngredients = true;
+    $container.append(
+        `<h3>Ingredients:</h3><br>
+		<select>
+			<option value='Ingredients' selected disabled>Ingredients</option>
+			<option value="milk">Milk</option>
+			<option value="eggs">Eggs</option>
+			<option value="flour">Flour</option>
+			<option value="tomatoes">Tomatoes</option>
+			<option value="potatoes">Potatoes</option>
+			<option value="paprika">Paprika</option>
+			<option value="cheese">Cheese</option>
+			<option value="sugar">Sugar</option>
+			<option value="oil">Oil</option>
+			<option value="salt">Salt</option>
+			<option value="meat">Meat</option>
+		</select>
+		<input id='ingredientAmmount' type='number' style='width: 100px'><br />
+		<button id='getIngredient'>Add Ingredient</button> 
+		<button id='next'>Next</button> `
+    );
+};
+
+
+let preparation = () => {
+    showPreparation = true;
+    $container.append(
+        `<h3>Preparation:</h3>
+		<label for='preparationTime'>Preparation Time</label>
+		<input id='preparationTime' type='number' style='width: 100px'><br>
+		<label for='preparationGuide'>Preparation Guide</label><br>
+		<textarea id='preparationGuide' rows='10' cols='30'></textarea><br>
+		<button id='saveRecipe'>Save Recipe</button> `
+    );
+};
+
+$(() => {
+    newRecipe();
+    recipesTable();
+
+    $('#newRecipe').on('click', () => {
+        $container.html('');
+        newRecipe();
+    });
+    $('#listOfRecipes').on('click', () => {
+        $container.html('')
+        $('#containerList').removeClass('hide');
+    });
+    $container.on('click', '#addRecipe', () => {
+        console.log('im clicked');
+        recipes.push(new Recipe($('#name').val(), $('#origin').val()));
+        if (!showIngredients)
+            getIngredients();
+        $('#addRecipe').toggle('.hide');
+    });
+    $container.on('click', '#getIngredient', () => {
+        if (recipes != [])
+            recipes[recipes.length - 1].ingredients[$('select option:selected').attr('value')] = $('#ingredientAmmount').val();
+    });
+    $container.on('click', '#next', () => {
+        $container.html("");
+        newRecipe();
+        if (!showPreparation)
+            preparation();
+        $('#addRecipe').toggle('.hide');
+        $('#next').toggle('.hide');
+        $('#getIngredient').toggle('.hide');
+    });
+    $container.on('click', '#saveRecipe', () => {
+        if (recipes != []) {
+            recipes[recipes.length - 1].preparationTime = $('#preparationTime').val();
+            recipes[recipes.length - 1].preparationGuide = $('#preparationGuide').val();
+            appendRecipe();
+            $container.html("");
+            newRecipe();
+            showIngredients = false;
+            showPreparation = false;
+        }
+    });
+    $('#containerList').on('click', 'deleteButton', () => {
+    	$('deleteButton').parent().parent().remove();
+    	console.log('clicked');
+    });
+});
